fc04ef0248c210f6644897edc3fc52c0e7b01b21
Çağatay ATAY <107729215+CagatayAtay@users.noreply.github.com>
2023-06-10 13:30:50 +0300

    content_page added

---
+++ lib/pages/content_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Content_page extends StatefulWidget {
+  const Content_page({super.key});
+
+  @override
+  State<Content_page> createState() => _Content_pageState();
+}
+
+class _Content_pageState extends State<Content_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Title"),
+      ),
+      body: Column(),
+    );
+  }
+}
